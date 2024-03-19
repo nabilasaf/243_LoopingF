0c713ce6a4ebf8dc5b6f027cf9a29b943b46c02c
nabilasaf <nabilasafitri861@gmail.com>
2024-03-19 15:48:22 +0700

    Prosedur&Fungsi

---
+++ Prosedur&Fungsi.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+string nama;
+int nAdidas, nPuma, nNB, nCompass, nNike;
+int hAdidas = 300, hNB = 250, hPuma = 150, hNike = 350;
+
+void input()
+{
+    cout << "Masukan jumlah Adidas = ";
+    cin >>nAdidas;
+    
+    cout << "Masukan jumlah Puma = ";
+    cin >>nPuma;
+
+    cout << "Maasukan jumlah New Balance = ";
+    cin >>nNB;
+
+    cout << "Masukan jumlah Nike = ";
+    cin >> nNike;
+
+}
+int totalHarga()
+{
+    return(nAdidas * hAdidas) + (nPuma * hPuma) + (nNB * hNB) + (nNike * hNike);
+}
+void display(){
+    cout << "Jumalah puma = " << nPuma << endl;
+    cout << "Jumlah Adidas = " << nAdidas << endl;
+    cout << endl;
+    cout << "Total Harga = Rp." << totalHarga() << endl;
+}
+
+int main()
+{
+    input();
+    display();
+
+}
